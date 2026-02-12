5f0e9624fa4afe568c5eb526b3e0fa6becfafc9f
Richard Darst <rkd@zgib.net>
2026-02-12 09:43:03 +0200

    Move extension to subdir

--- make-archive.sh
+++ make-archive.sh
@@ -1,6 +1,7 @@
 # Create the zipfile that is the extension.
 # This does not yet work, since the .xpi file would have to be signed.
-zip firefox-ext-delete-outlook-cookies.xpi \
+cd extension
+zip ../firefox-ext-delete-outlook-cookies.xpi \
     manifest.json \
     background.js \
     icon.ico
